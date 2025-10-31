63df64dee48c384baebe81b0227e42f33738eaa6
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2025-11-01 02:03:30 +0800

    Commit all

---
+++ scripts/conversion.js
@@ -0,0 +1,32 @@
+
+const button = document.getElementById("convert-btn");
+
+
+button.addEventListener("click", computeConversion);
+button.addEventListener("click", computeConversion);
+function computeConversion() {
+    const inputValue = document.getElementById("inputValue").value;
+    const conversionType = document.getElementById("conversion-types").value;   
+    let result;
+
+    switch (conversionType) {
+        case "ctof":
+            result = ((inputValue * 1.8) + 32).toFixed(2);
+            document.getElementById("result").innerHTML = `${inputValue} Celsius = ${result} Fahrenheit`;
+            break;
+        case "ftoc":
+            result = ((inputValue - 32) / 1.8).toFixed(2);
+            document.getElementById("result").innerHTML = `${inputValue} Fahreinheit = ${result} Celsius`;
+            break;
+        case "mtoft":
+            result = ((inputValue * 3.28084).toFixed(2));
+            document.getElementById("result").innerHTML = `${inputValue} Meters = ${result} Feet`;
+            break;
+        case "fttom":
+            result = ((inputValue * 0.3048).toFixed(2));
+            document.getElementById("result").innerHTML = `${inputValue} Feet = ${result} Meters`;
+            break;
+        default:
+            document.getElementById("resultInput").value = "Invalid conversion type.";
+    } 
+}
