87c9c905b8c8aaa0348a19e79041b373f390377d
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2025-11-07 18:41:07 +0800

    Made the css better

--- scripts/simplepayroll.js
+++ scripts/simplepayroll.js
@@ -1,16 +1,15 @@
-const button = document.querySelector(".add-button");
-const employeeName = document.getElementById("")
-button.addEventListener("click", addToPayroll);
 
+/*
+const table = [];
 
 function addToPayroll() {
     let employeeInfo = {
-        name:
-        daysWorked:
-        dailyRate:
-        grossPay:
-        deduction:
+        name: employeeName,
+        daysWorked: daysWorked,
+        dailyRate: dailyRate,
+        deduction: deduction
     }
+    table.push(employeeInfo);
 }
 
 document.querySelector(".table").innerHTML = `<table>
@@ -37,3 +36,108 @@ document.querySelector(".table").innerHTML = `<table>
             <td>Gross Pay</td>
         </tr>
     </table>`
+*/
+const button = document.querySelector(".add-button");
+const deleteButton = document.querySelector(".delete-button");
+let table = [];
+
+button.addEventListener("click", addToPayroll);
+deleteButton.addEventListener("click", deleteEmployee);
+
+function initTable() {
+    table = [];
+    document.querySelector(".table").innerHTML = "";
+}
+
+function clearTable() {
+    initTable();
+}
+
+function addToPayroll() {
+    const employeeName = document.getElementById("employeeName").value;
+    const daysWorked = document.getElementById("daysWorked").value;
+    const dailyRate = document.getElementById("dailyRate").value;
+    const deduction = document.getElementById("deduction").value;
+
+    if (!employeeName || !daysWorked || !dailyRate || !deduction) {
+        alert("Incomplete info. Please Try Again");
+    } else {
+
+    let employeeInfo = {
+        "name": employeeName,
+        "daysWorked": daysWorked,
+        "dailyRate": dailyRate,
+        "deduction": deduction
+    }
+
+    table.push(employeeInfo);
+    showPayroll();
+}
+}
+
+function computeTotal () {
+    let totalNetPay;
+    totalNetPay += netPay;
+}
+
+function deleteEmployee() {
+    let deleteEmployee = document.querySelector(".employee-number").value;
+
+    if (deleteEmployee < 0 || deleteEmployee > table.length) {
+        alert("Invalid Employee Number");
+        document.querySelector(".employee-number").value = '';
+    } else {
+    table.splice(deleteEmployee - 1, 1);
+    showPayroll();
+    }
+}
+
+function showPayroll() {
+    const thead = `
+        <thead>
+            <tr>
+            <th>No.</th>
+            <th>Employee Name</th>
+            <th>Days Worked</th>
+            <th>Daily Rate</th>
+            <th>Gross Pay</th>
+            <th>Deduction</th>
+            <th>Net Pay</th>
+        </tr>
+        </thead>`;
+    let tbody = '';
+    let totalNetPay = 0; // Initialize total
+    
+    for (let i = 0; i < table.length; i++) {
+        const employee = table[i];
+        const grossPay = employee.daysWorked * employee.dailyRate;
+        const netPay = grossPay - employee.deduction;
+        
+        totalNetPay += netPay;
+
+    tbody += `<tr>
+            <td>${i+1}</td>
+            <td>${employee.name}</td>
+            <td>${employee.daysWorked}</td>
+            <td>${employee.dailyRate}</td>
+            <td>${grossPay}</td>
+            <td>${employee.deduction}</td>
+            <td>${netPay}</td>
+            </tr>`;
+    }
+
+    let tfooter = `
+        <tfoot>
+            <tr>
+                <th class="footer" colspan='5'>Total Net Pay</th>
+                <th class="footer" colspan='2'>${totalNetPay}</th>
+            </tr>
+        </tfoot>
+    `
+
+    document.querySelector(".table").innerHTML = `<table>
+    ${thead}<tbody>${tbody}</tbody>${tfooter}</table>`;
+    
+}
+
+showPayroll();
